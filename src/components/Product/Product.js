cbac17a083f0edb7087bdb4c01b79a540e434e80
syltur <sylwia.tur99@gmail.com>
2023-02-09 09:50:18 +0100

    add existing files

---
+++ src/components/Product/Product.js
@@ -0,0 +1,66 @@
+import styles from './Product.module.scss';
+import clsx from 'clsx';
+import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import shortid from 'shortid';
+
+const Product = (props) => {
+  const [currentColor, setCuurentColor] = useState(props.data.colors[0]);
+  const [currentSize, setCurrentSize] =useState(props.data.sizes[0].name);
+  const prepareColorClassName = (color) => {
+    return styles[
+      'color' + color[0].toUpperCase() + color.substr(1).toLowerCase()
+    ];
+  };
+  return (
+    <article className={styles.product}>
+      <div className={styles.imageContainer}>
+        <img 
+          className={styles.image}
+          alt="Kodilla shirt"
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.data.name}--${currentColor}.jpg`} />
+      </div>
+      <div>
+        <header>
+          <h2 className={styles.name}>{props.data.title}</h2>
+          <span className={styles.price}>Price: {props.data.basePrive}$</span>
+        </header>
+        <form>
+          <div className={styles.sizes}>
+            <h3 className={styles.optionLabel}>Sizes</h3>
+            <ul className={styles.choices}>
+            {props.data.sizes.map((size) => (
+                <li key={shortid()}>
+                  <button type='button' className={(size.name, currentSize === size.name && styles.active)}
+                    onClick={() => setCurrentSize(size.name)}>{`${size.name}`}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.colors}>
+            <h3 className={styles.optionLabel}>Colors</h3>
+            <ul className={styles.choices}>
+              {props.data.colors.map((color) => (
+                <li key={shortid()}>
+                  <button type='button' className={clsx(prepareColorClassName(color), currentColor === color && styles.active)}
+                    onClick={() => setCuurentColor(color)}></button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <Button className={styles.button}>
+            <span className="fa fa-shopping-cart" />
+          </Button>
+        </form>
+      </div>
+    </article>
+  )
+};
+
+
+Product.propTypes = { props: PropTypes.any };
+
+export default Product;
