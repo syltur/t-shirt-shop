03dbe522d7838fe43dce9ee1ad70d81b2a505331
syltur <sylwia.tur99@gmail.com>
2023-02-21 08:35:57 +0100

    add ProductForm files

---
+++ src/components/ProductForm/ProductForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '../Button/Button';
+import OptionColor from '../OptionColor/OptionColor';
+import OptionSize from '../OptionSize/OptionSize';
+import styles from './ProductForm.module.scss';
+
+const ProductForm = (props) => {
+  return (
+    <form onSubmit={props.productToBasket}>
+      <OptionSize
+        sizes={props.sizes}
+        size={props.size}
+        currentSize={props.currentSize}
+        setCurrentSize={props.setCurrentSize}
+        price={props.price}
+        additionalPrice={props.additionalPrice}
+        setCurrentPrice={props.setCurrentPrice}
+      />
+      <OptionColor
+        colors={props.colors}
+        currentColor={props.currentColor}
+        setCurrentColor={props.setCurrentColor}
+      />
+      <Button className={styles.button}>
+        <span className='fa fa-shopping-cart' />
+      </Button>
+    </form>
+  );
+};
+
+export default ProductForm;
